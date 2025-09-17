b9693b0700ff93cb9b3032fa5a063220353d60dc
Rayan <rayanyasin9@yahoo.com>
2025-09-17 11:42:30 -0500

    Add review deletion and campground cascade delete middleware

--- models/campground.js
+++ models/campground.js
@@ -14,4 +14,14 @@ const CampgroundSchema = new Schema({
     }] 
 });
 
+CampgroundSchema.post('findOneAndDelete', async function(doc) {
+    if(doc){
+        await review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
 module.exports = mongoose.model('Campground', CampgroundSchema);
