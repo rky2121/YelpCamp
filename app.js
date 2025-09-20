13d9c36a933dc286c27aaaaf52cbcd5825fd0a58
Rayan <rayanyasin9@yahoo.com>
2025-09-19 21:07:48 -0500

    Restructure to serve static assets from public directory

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const validateCampground = (req, res, next) => {
     const {error} = campgroundSchema.validate(req.body);
