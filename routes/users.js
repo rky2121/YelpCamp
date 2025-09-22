368524487ac6bc4963cddd88c434febe195ac5c3
Rayan <rayanyasin9@yahoo.com>
2025-09-22 11:11:08 -0500

    Add user model, configure passport local auth, create register form and route

---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/user');
+const passport = require('passport');
+
+router.get('/register', (req, res) => {
+    res.render('users/register');
+});
+
+router.post('/register', catchAsync(async (req, res, next) => {
+    try {
+        const {username, email, password} = req.body;
+        const user = new User({username, email});
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        });
+    } catch(e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
+
+module.exports = router;
