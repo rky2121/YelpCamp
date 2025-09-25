838d1c66254cd4bb1f1c4f78a92f74e592ac3e26
Rayan <rayanyasin9@yahoo.com>
2025-09-25 15:09:35 -0500

    Add campground authorship and authorization checks (show/hide edit & delete, enforce author permissions)

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '68d4d59d71a738b80cd8646e',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title:  `${sample(descriptors)} ${sample(places)}`,
             image: `https://picsum.photos/400?random=${Math.random()}`,
