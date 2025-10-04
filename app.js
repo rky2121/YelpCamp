4c67782fd8858e5be54bffb0d777ef99c0b42d6e
Rayan <rayanyasin9@yahoo.com>
2025-10-04 02:00:32 -0500

    re-enable helmet after debugging

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
-// app.use(helmet());
+app.use(helmet());
 
 const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
